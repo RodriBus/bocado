278d4f3720410b5858ced9a3cedc4b647ccd066d
Diego Rodríguez Suárez-Bustillo <diego@rodribus.com>
2017-02-27 20:06:51 +0100

    add basic setup

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+
+const api = require('./api/index');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false,
+}));
+
+app.use('/', api);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json(err);
+});
+
+module.exports = app;
