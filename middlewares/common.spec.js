e9f9ab8281a215153861770b8135b9e73c9a20f7
Diego Rodríguez Suárez-Bustillo <diego@rodribus.com>
2017-02-28 20:51:23 +0100

    improve common middlewares tests

--- middlewares/common.spec.js
+++ middlewares/common.spec.js
@@ -3,11 +3,10 @@ const app = require('../bin/www');
 const chai = require('chai');
 const spies = require('chai-spies');
 
-const request = require('supertest');
-
 const middlewares = require('./common');
 
 const expect = chai.expect;
+const assert = chai.assert;
 
 const token = '0';
 const command = 'bocata';
@@ -20,31 +19,104 @@ process.env.NODE_ENV = 'test';
 process.env.BOCADO_TOKEN = token;
 process.env.BOCADO_COMMAND = command;
 
-describe('validatePayload middleware', () => {
-  const { validatePayload } = middlewares;
-  it('should return a function()', () => {
-    expect(validatePayload).to.be.a.Function;
+describe('common middlewares', () => {
+  const simpleSpy = chai.spy();
+  const errorOkSpy = chai.spy((err) => {
+    if (err instanceof Error) {
+      assert.isOk('everything', 'everything is ok');
+    } else {
+      assert.fail(err, undefined, 'No error was passed to middleware');
+    }
   });
-  it('should accept three arguments', () => {
-    expect(validatePayload.length).to.equal(3);
+  const errorFailSpy = chai.spy((err) => {
+    if (err instanceof Error) {
+      assert.fail(err, undefined, 'Error was passed to middleware');
+    } else {
+      assert.isOk('everything', 'everything is ok');
+    }
   });
-  it('should validate token is sent', () => {
-    const req = { body: { command, user_id } };
-    const spy = chai.spy();
-    validatePayload(req, {}, spy);
-    expect(spy).to.have.been.called.once();
+
+  beforeEach(() => {
+    simpleSpy.reset();
+    errorOkSpy.reset();
+    errorFailSpy.reset();
   });
-  it('should validate command is sent', () => {
-    const req = { body: { token, user_id } };
-    const spy = chai.spy();
-    validatePayload(req, {}, spy);
-    expect(spy).to.have.been.called.once();
+
+  describe('apply middlewares', () => {
+    const { apply } = middlewares;
+    const middlewaresLength = Object.keys(middlewares).length - 1;
+    it('should apply all middlewares', () => {
+      expect(apply).to.be.a.Function;
+      apply({ use: simpleSpy });
+      expect(simpleSpy).to.have.been.called.exactly(middlewaresLength);
+    });
   });
-  it('should validate user_id is sent', () => {
-    const req = { body: { token, command } };
-    const spy = chai.spy();
-    validatePayload(req, {}, spy);
-    expect(spy).to.have.been.called.once();
+
+  describe('validatePayload middleware', () => {
+    const { validatePayload } = middlewares;
+    it('should return a function()', () => {
+      expect(validatePayload).to.be.a.Function;
+    });
+    it('should accept three arguments', () => {
+      expect(validatePayload.length).to.equal(3);
+    });
+    it('should validate token is sent', () => {
+      const req = { body: { command, user_id } };
+      validatePayload(req, {}, errorOkSpy);
+      expect(errorOkSpy).to.have.been.called.once();
+    });
+    it('should validate command is sent', () => {
+      const req = { body: { token, user_id } };
+      validatePayload(req, {}, errorOkSpy);
+      expect(errorOkSpy).to.have.been.called.once();
+    });
+    it('should validate user_id is sent', () => {
+      const req = { body: { token, command } };
+      validatePayload(req, {}, errorOkSpy);
+      expect(errorOkSpy).to.have.been.called.once();
+    });
+  });
+
+  describe('validateToken middleware', () => {
+    const { validateToken } = middlewares;
+    it('should return a function()', () => {
+      expect(validateToken).to.be.a.Function;
+    });
+    it('should accept three arguments', () => {
+      expect(validateToken.length).to.equal(3);
+    });
+    it('should validate token sent equals to configured one', () => {
+      const req = { body: { token } };
+      validateToken(req, {}, errorFailSpy);
+      expect(errorFailSpy).to.have.been.called.once();
+    });
+    it('should fail if token is not sent', () => {
+      const req = { body: {} };
+      validateToken(req, {}, errorOkSpy);
+      expect(errorOkSpy).to.have.been.called.once();
+    });
+  });
+
+  describe('validateCommand middleware', () => {
+    const { validateCommand } = middlewares;
+    it('should return a function()', () => {
+      expect(validateCommand).to.be.a.Function;
+    });
+    it('should accept three arguments', () => {
+      expect(validateCommand.length).to.equal(3);
+    });
+    it('should validate token sent equals to configured one', () => {
+      const req = { body: { command: `/${command}` } };
+      validateCommand(req, {}, errorFailSpy);
+      expect(errorFailSpy).to.have.been.called.once();
+    });
+    it('should fail if token is not sent', () => {
+      const req = { body: {} };
+      validateCommand(req, {}, errorOkSpy);
+      expect(errorOkSpy).to.have.been.called.once();
+    });
   });
 });
+
+
 
